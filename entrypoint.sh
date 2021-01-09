9b9b6da623fcf99ea6d510b06b77c2d1073bf266
Denis <den.zalman76@gmail.com>
2021-01-10 00:59:19 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,9 @@ aws configure set default.region "${INPUT_AWS_REGION}"
 ######
 
 echo "SSM send command..."
-echo $(aws ssm send-command --document-name "AWS-RunShellScript" --parameters "commands=${INPUT_COMMAND}" --instance-ids="${INPUT_INSTANCE_IDS}")
+# echo $(aws ssm send-command --document-name "AWS-RunShellScript" --parameters "commands=${INPUT_COMMAND}" --instance-ids="")
+
+echo $(aws ssm send-command --document-name "AWS-RunShellScript" --targets "[{"Key":"InstanceIds","Values":["${INPUT_INSTANCE_IDS}"]}]" --parameters "{"workingDirectory":["/home/ubuntu"],"commands":["${INPUT_COMMAND}"]}" --comment "Actions" --timeout-seconds 30)
 
 ######
 
