4c07f460b34e171e70a0bf5886bbbbd8d3ad24c4
Denis <den.zalman76@gmail.com>
2021-01-10 01:11:38 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,9 +9,12 @@ aws configure set default.region "${INPUT_AWS_REGION}"
 ######
 
 echo "SSM send command..."
-# echo $(aws ssm send-command --document-name "AWS-RunShellScript" --parameters "commands=${INPUT_COMMAND}" --instance-ids="")
-
-echo $(aws ssm send-command --document-name "AWS-RunShellScript" --targets "[{"Key":"InstanceIds","Values":["${INPUT_INSTANCE_IDS}"]}]" --parameters "{"workingDirectory":["/home/ubuntu"],"commands":["${INPUT_COMMAND}"]}" --comment "Actions" --timeout-seconds 30)
+# echo $(aws ssm send-command --document-name "AWS-RunShellScript" --parameters "commands=${INPUT_COMMAND}" --instance-ids="${INPUT_INSTANCE_IDS}")
+parameters="{\"workingDirectory\":[\"${INPUT_WORKING_DIRECTORY}\"],\"commands\":[\"${INPUT_COMMAND}\"]}"
+targets="[{\"Key\":\"InstanceIds\",\"Values\":[\"${INPUT_INSTANCE_IDS}\"]}]" 
+echo "parameters: $parameters"
+echo "targets: $targets"
+echo $(aws ssm send-command --document-name "AWS-RunShellScript" --targets "${targets}" --parameters "${parameters}" --comment "Github Actions" --timeout-seconds 30)
 
 ######
 
